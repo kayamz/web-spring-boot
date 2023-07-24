23f74f621c6812b0497ff9c4ad95ece923d92976
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-07-25 02:35:45 +0900

    제목, 내용으로 검색 form

--- myhome/src/main/java/com/kaya/myhome/repository/BoardRepository.java
+++ myhome/src/main/java/com/kaya/myhome/repository/BoardRepository.java
@@ -1,8 +1,15 @@
 package com.kaya.myhome.repository;
 
 import com.kaya.myhome.model.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+import java.util.List;
 
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+
+    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
