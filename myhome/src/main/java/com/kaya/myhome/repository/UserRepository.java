715e4663053760d203a33307f6e4075d700e88b0
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-07-28 19:59:59 +0900

    회원가입, 로그인 페이지 추가

---
+++ myhome/src/main/java/com/kaya/myhome/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kaya.myhome.repository;
+
+import com.kaya.myhome.model.Board;
+import com.kaya.myhome.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
